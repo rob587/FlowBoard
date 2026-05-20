dbfbb4b17e5aa0fe6085e7d3e93c98d8805523a4
rob587 <robertocamm02@yahoo.com>
2026-05-20 14:20:16 +0200

    finalized index.js and tested call on postman

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,16 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// status
+
+app.get("/", (req, res) => {
+  res.json({ status: "OK", timestamp: new Date() });
+});
+
+const PORT = process.env.PORT || 5000;
+console.log(`tentativo di ingresso sulla porta: ${PORT} `);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server aperto in http://0.0.0.0:${PORT}`);
+});
