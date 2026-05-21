f9da89e437d1a83a794b809b7ea7c72efabc88a1
rob587 <robertocamm02@yahoo.com>
2026-05-21 11:13:26 +0200

    implement getAllBoards function to retrieve boards from database

--- backend/models/boardModel.js
+++ backend/models/boardModel.js
@@ -5,3 +5,15 @@
 // deleteBoard(id) - DELETE WHERE id
 
 const pool = require("../db");
+
+const getAllBoards = async () => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM boards ORDER BY created_at DESC",
+    );
+    return result.rows;
+  } catch (err) {
+    console.error("errore nel recupero delle boards:", err);
+    throw err;
+  }
+};
