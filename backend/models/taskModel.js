1b20f16d11208907a708bb5b398bb9e46424b0d6
rob587 <robertocamm02@yahoo.com>
2026-05-22 22:31:31 +0200

    add: implement getTaskById and createTask functions for task management

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -14,3 +14,26 @@ const getTasksByBoardId = async (boardId) => {
     throw err;
   }
 };
+
+const getTaskById = async (id) => {
+  try {
+    const result = await pool.query("SELECT * FROM tasks WHERE id = $1", [id]);
+    return result.rows[0];
+  } catch (err) {
+    console.log("errore nel recupero della task:", err);
+    throw err;
+  }
+};
+
+const createTask = async (boardId, title, description) => {
+  try {
+    const result = await pool.query(
+      "INSERT INTO tasks (board_id, title, description) VALUES ($1, $2, $3) RETURNING *",
+      [boardId, title, description],
+    );
+    return result.rows[0];
+  } catch (err) {
+    console.log("errore nella creazione della task:", err);
+    throw err;
+  }
+};
