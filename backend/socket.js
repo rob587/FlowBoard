2b90ef4d3bad6782c947186964b42f6ac5c58347
rob587 <robertocamm02@yahoo.com>
2026-05-26 12:02:12 +0200

    add: implement socket setup for real-time communication

---
+++ backend/socket.js
@@ -0,0 +1,18 @@
+const socketIO = require("socket.io");
+
+const setupSocket = (server) => {
+  const io = socketIO(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Utente collegato:", socket.id);
+    socket.on("join:board", (boardId) => {
+      socket.join(`board:${boardId}`);
+      console.log(`Utente ${socket.id} si è unito alla board ${boardId}`);
+    });
+  });
+};
