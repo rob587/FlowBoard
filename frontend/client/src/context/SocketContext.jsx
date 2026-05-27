97ceab0c0194c781a678b1d8dc320c2d0c322264
rob587 <robertocamm02@yahoo.com>
2026-05-27 12:16:27 +0200

    add: implement SocketProvider for real-time task updates and connection management

--- frontend/client/src/context/SocketContext.jsx
+++ frontend/client/src/context/SocketContext.jsx
@@ -9,4 +9,20 @@ export const SocketProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
   const [boards, setBoards] = useState([]);
   const [currentBoardId, setCurrentBoardId] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:5000");
+    setSocket(newSocket);
+
+    console.log("connessione a Socket.IO..");
+
+    newSocket.on("connect", () => {
+      console.log("Connesso al Server!");
+    });
+
+    newSocket.on("task:created", (task) => {
+      console.log("Task Creata!:", task);
+      setTasks((prev) => [...prev, task]);
+    });
+  }, []);
 };
